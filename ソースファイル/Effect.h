bce84bc101b50acf3c03869397e813debe996558
T-T-web-o <toppy022755@gmail.com>
2026-05-14 13:43:42 +0900

    コメント追加

--- ソースファイル/Effect.h
+++ ソースファイル/Effect.h
@@ -3,18 +3,25 @@
 class Effect
 {
 public:
+	// 現在位置
+	int x, y;
 
-	int x;
-	int y;
+	// 使用する画像
 	int image;
 
+	// 描画時間
 	int timer;
 
+	// 削除フラグ
 	bool isDead;
 
+	// コンストラクタ(x座標、ｙ座標、使用する画像)
 	Effect(int x, int y, int image);
 
+	// 更新処理
 	void Update();
+
+	// 描画処理
 	void Draw();
 };
 
